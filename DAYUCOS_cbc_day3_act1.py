5fe97496c285ffc6676be634113563a7a538c2cf
neccajoy11 <neccadayucos@gmail.com>
2025-02-18 21:11:28 -0800

    initial commit

---
+++ DAYUCOS_cbc_day3_act1.py
@@ -0,0 +1,10 @@
+name = "cODINGboOTHCamP123"
+
+print("Is Alpha: " , name.isalpha())
+print("Is isdigit: " , name.isdigit())
+print("Capatilize of First word: " , name.capitalize())
+print("Lowercase: " , name.casefold())
+print("Is Isuppper: " , name.isupper())
+print(name.capitalize().center(50))
+print(name.capitalize().ljust(50))
+print(name.capitalize().rjust(50))
